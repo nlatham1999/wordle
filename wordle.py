6c0881cbf7fd80804a655a151742e99f61a3bf2d
Nick Latham <nlatham@zagmail.gonzaga.edu>
2022-01-19 19:36:57 -0800

    updated word list

--- wordle.py
+++ wordle.py
@@ -4,8 +4,8 @@ o = open("words.txt", "w")
 lines = i.readlines()
 
 in_word = ''
-in_location = {'a': 0, 'b': 1, 'e': 3}
-not_in_word = 'diuftrldmocks'
+in_location = {'p': 0, 'r': 1, 'o': 2}
+not_in_word = 'adieufnts'
 not_in_location = {}
 
 def is_in_word(a, b):
@@ -35,7 +35,7 @@ def is_not_in_location(a, b):
 words = []
 for x in lines:
     # if len(x) == 6:
-
+    # if '-' not in x and '.' not in x and '\'' not in x:
     if is_in_word(in_word, x) and is_in_location(in_location, x) and is_not_in_word(not_in_word, x) and is_not_in_location(not_in_location, x):
         words.append(x.lower())
 
